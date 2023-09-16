18a8b061680f1dc6bd10703fa96b8bea8f87f815
Lgup8 <116928284+Lgup8@users.noreply.github.com>
2023-09-16 18:19:14 -0500

    Update game_config.py

--- game_config.py
+++ game_config.py
@@ -45,7 +45,8 @@ level_config = """
             "bubble1":{"text":"What?", "x":7, "y":5},
             "bubble2":{"text":"Strange", "x":16, "y":11},
             "bubble3":{"text":"Monsters?", "x":13, "y":3},
-            "bubble4":{"text":"hi!", "x":10, "y":6}
+            "bubble4":{"text":"Meow!", "x":47, "y":12},
+            "bubble5":{"text":"hi!", "x":10, "y":6}
         },
         "chests": {
             "chest1":{"x":18, "y":6, "file":"chest_golden_open_full.png", "visible":true},
@@ -85,7 +86,7 @@ level_config = """
             "bubble2":{"text":"Strange", "x":11, "y":11},
             "bubble3":{"text":"Monsters?", "x":13, "y":3},
             "bubble4":{"text":"Meow!", "x":47, "y":12},
-            "bubble5":{"text":"hi! ", "x":15, "y":6}
+            "bubble5":{"text":"hi!", "x":15, "y":6}
         },
         "chests": {
             "chest1":{"x":4, "y":14, "file":"chest_golden_open_full.png", "visible":true},
