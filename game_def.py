772a5dd638ce480c45997ad6a24cdbb23640a5ec
Lgup8 <116928284+Lgup8@users.noreply.github.com>
2023-09-17 09:46:50 -0500

    changed sizing and positioning text boxes

--- game_def.py
+++ game_def.py
@@ -338,7 +338,7 @@ def create_text_bubble_html(text, x, y):
     """
     return f"""
         <div class="container_text" style="position: relative; 
-        grid-column-start: {x}; grid-row-start: {y+2}; grid-column-end: {x-8};">
+        grid-column-start: {x}; grid-row-start: {y+2}; grid-column-end: {x-5};">
             <img src="https://raw.githubusercontent.com/TomJohnH/streamlit-dungeon/main/graphics/other/message.png">
             <div style="position: absolute; top: 40%; left: 50%; transform: translate(-50%, -50%); font-size:0.875rem;">{text}</div>
         </div>
